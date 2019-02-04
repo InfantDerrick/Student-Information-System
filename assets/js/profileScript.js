18cdf55a6d09fec3f93647b32e754646e335de08
Infant Derrick Gnanasusairaj <infant.derrick.gnanasusairaj@gmail.com>
2019-02-03 21:29:07 -0500

    photoURL

--- assets/js/profileScript.js
+++ assets/js/profileScript.js
@@ -1,6 +1,6 @@
 firebase.auth().onAuthStateChanged(function(user){
   if(user){
-    $(".profilePic").attr("src", user.profilePic);
+    $(".profilePic").attr("src", user.photoURL);
   }else{
     window.open("./login.html", "_self");
   }
